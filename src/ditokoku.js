888df273204bf13039732af65f532c01f4f8ee25
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-10 00:19:58 +0700

    port

--- src/ditokoku.js
+++ src/ditokoku.js
@@ -1,9 +1,8 @@
 import app from "./app";
 import {utcToZonedTime, format} from "date-fns-tz";
 
-// const port = process.env.APP_PORT || 80;
-const port = 80;
-
-app.listen(port, ()=>{
-    console.log (`Server is up on port ${port} at ${format(utcToZonedTime(Date.now(),process.env.APP_TIMEZONE), 'yyyy-MM-dd HH:mm:ss.SSS',{timeZone: process.env.APP_TIMEZONE})}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, err => {
+    if(err) throw err;
+    console.log (`Server is up on port ${PORT} at ${format(utcToZonedTime(Date.now(),process.env.APP_TIMEZONE), 'yyyy-MM-dd HH:mm:ss.SSS',{timeZone: process.env.APP_TIMEZONE})}`)
+});
