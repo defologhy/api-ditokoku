944d5a198d8a2ef841440cfdffa4c859b33fdeb5
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-10 00:13:33 +0700

    port on serve

--- src/ditokoku.js
+++ src/ditokoku.js
@@ -1,7 +1,7 @@
 import app from "./app";
 import {utcToZonedTime, format} from "date-fns-tz";
 
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 80;
 
 app.listen(port, ()=>{
     console.log (`Server is up on port ${port} at ${format(utcToZonedTime(Date.now(),process.env.APP_TIMEZONE), 'yyyy-MM-dd HH:mm:ss.SSS',{timeZone: process.env.APP_TIMEZONE})}`)
