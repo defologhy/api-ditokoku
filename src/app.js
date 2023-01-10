21b8c875cf62c9e67b0cb8930173e7fd06599830
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-10 15:26:05 +0700

    cors setting

--- src/app.js
+++ src/app.js
@@ -12,33 +12,51 @@ const app = express();
 app.use(express.json());
 
 // Add headers
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    // const allowedOrigins = ['http://localhost:3001', 'http://localhost:3002'];
-    const allowedOrigins = ['https://ditokoku.vercel.app/', 'https://admin-ditokoku.vercel.app/'];
-    const origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader('Access-Control-Allow-Origin', origin);
+//     // Website you wish to allow to connect
+//     // const allowedOrigins = ['http://localhost:3001', 'http://localhost:3002'];
+//     const allowedOrigins = ['https://ditokoku.vercel.app/', 'https://admin-ditokoku.vercel.app/'];
+//     const origin = req.headers.origin;
+//     if(allowedOrigins.indexOf(origin) > -1){
+//         res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
     }
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+    next()
+})
+//Cors Configuration - End
 
 app.use(compression()); //Compress all routes
 app.use(helmet());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 //routes middleware
 app.use("/api/v1/resellers", resellersRoutesV1);
@@ -47,4 +65,4 @@ app.use("/api/v1/common", commonRoutesV1);
 
 databaseSynchronize();
 
-export { app as default}
+export { app as default }
