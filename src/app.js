4798a18dc78c17ca5eb1d4b2f9fbd04045a3b3e3
dimas.syarief <defitramuhamadyasin20@gmail.com>
2023-01-17 23:53:25 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -14,24 +14,7 @@ import databaseSynchronize from "./databases/database-synchronize";
 const app = express();
 const cors = require('cors')
 
-  // app.use(cors())
-//Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-//Cors Configuration - End
+app.use(cors())
 
 const fileupload = require("express-fileupload");
 const bodyParser = require('body-parser');
